19924aadcac1bf5181cf60439fe3720611bb0705
Yuhan Zheng <yuhanzh22@gmail.com>
2023-11-07 21:48:47 +0800

    break into components

---
+++ src/NewItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NewItem() {
+  return (
+    <div>
+      <p>New item</p>
+      <input type="text" />
+      <button>Add</button>
+    </div>
+  );
+}
+
+export default NewItem;
---
+++ src/TodoList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TodoList() {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input type="checkbox" name="" id="" />
+      <label htmlFor="">Do Laundry</label>
+    </div>
+  );
+}
+
+export default TodoList;
