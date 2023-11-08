b3bc20410508b5a12fb5343256aadcf16dc7dbd8
Yuhan Zheng <yuhanzh22@gmail.com>
2023-11-08 10:49:52 +0800

    attempt

--- src/NewItem.jsx
+++ src/NewItem.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+const [item, addItem] = React.useState("");
+
+function handleAddItem(event) {
+  event.preventDefault();
+}
+
 function NewItem() {
   return (
-    <form className="new-item-form">
+    <form onSubmit={handleAddItem} className="new-item-form">
       <label htmlFor="item">New Item</label>
-      <input type="text" id="item" />
+      <input type="text" id="item" required={true} />
       <button className="btn">Add</button>
     </form>
   );
