211f829d1004fafbd9768e835f8c728655ded161
Yuhan Zheng <yuhanzh22@gmail.com>
2023-11-16 18:37:45 +0800

    todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,63 @@
 import "./styles.css";
 import AddNewItemForm from "./AddNewItemForm.jsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
+
+    return JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(items));
+  }, [items]);
 
   function handleAddItem(label) {
     const newItem = {
       label,
       id: crypto.randomUUID(),
+      completed: false,
     };
     const nextItems = [...items, newItem];
     setItems(nextItems);
+    console.log(newItem);
+    console.log(newItem.completed);
   }
 
   function handleDeleteItem(id) {
     setItems(items.filter((item) => item.id !== id));
   }
+  //update the todo to change the id of the todo I pass in here to be completed base on this flag
+  function toggleTodo(id, completed) {
+    setItems((items) => {
+      return items.map((item) => {
+        if (item.id === id) {
+          item.completed = completed;
+          return { ...item, completed };
+        }
+
+        return item;
+      });
+    });
+  }
+
   return (
     <>
       <AddNewItemForm handleAddItem={handleAddItem} />
       <div>
         <h1 className="header">Todo List</h1>
         <ul className="list">
-          {items.map(({ id, label }) => (
+          {items.map(({ id, label, completed }) => (
             <li key={id}>
               <label>
-                <input type="checkbox" id={id} />
+                <input
+                  type="checkbox"
+                  checked={completed}
+                  onChange={(e) => toggleTodo(id, e.target.checked)}
+                  id={id}
+                />
                 {label}
               </label>
               <button
