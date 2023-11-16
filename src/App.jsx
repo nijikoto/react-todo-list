23dca87855c6f2a358cd9d75e54987e61170ef96
Yuhan Zheng <yuhanzh22@gmail.com>
2023-11-16 11:23:47 +0800

    functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./styles.css";
 import AddNewItemForm from "./AddNewItemForm.jsx";
-import TodoList from "./TodoList.jsx";
 import { useState } from "react";
 
 function App() {
@@ -9,31 +8,34 @@ function App() {
   function handleAddItem(label) {
     const newItem = {
       label,
-      id: Math.random(),
+      id: crypto.randomUUID(),
     };
     const nextItems = [...items, newItem];
     setItems(nextItems);
   }
 
+  function handleDeleteItem(id) {
+    setItems(items.filter((item) => item.id !== id));
+  }
   return (
     <>
       <AddNewItemForm handleAddItem={handleAddItem} />
       <div>
         <h1 className="header">Todo List</h1>
         <ul className="list">
-          <div>
-            <ul className="list">
-              {items.map(({ id, label }) => (
-                <li key={id}>
-                  <label>
-                    <input type="checkbox" />
-                    {label}
-                  </label>
-                  <button className="btn btn-danger">Delete</button>
-                </li>
-              ))}
-            </ul>
-          </div>
+          {items.map(({ id, label }) => (
+            <li key={id}>
+              <label>
+                <input type="checkbox" id={id} />
+                {label}
+              </label>
+              <button
+                className="btn btn-danger"
+                onClick={() => handleDeleteItem(id)}>
+                Delete
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
     </>
